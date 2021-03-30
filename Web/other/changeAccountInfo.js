4ca5ee671c9a22509cb3df8031b17cf3c8a0aad8
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-03-30 10:50:15 +0200

    html and start on js for" change account infromation " page

---
+++ Web/other/changeAccountInfo.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("changeInfoForm"); //form that holds the inputs thats used to change account information
+const name = document.getElementById("name"); //inputted name
+const username = document.getElementById("username"); //inputted username
+const password = document.getElementById("password"); //inputted password
+const address = document.getElementById("address"); //inputted address
+const phonenumber = document.getElementById("phone"); //inputted phonenumber
+const errorMessages = document.getElementById("errorMessage"); //div that holds error messages
+let message = []; //Array that holds errorMessages
