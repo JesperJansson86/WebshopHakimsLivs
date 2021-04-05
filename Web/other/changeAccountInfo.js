4ac7027dcff7cdc534030b4a5165a26af1d6d9de
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-05 18:15:00 +0200

    You can get to success now, but i do not yet check the password matching and other checks probably (theres a lot and im gonna get to them now)

--- Web/other/changeAccountInfo.js
+++ Web/other/changeAccountInfo.js
@@ -1,60 +1,61 @@
-importScripts()
 const form = document.getElementById("changeInfoForm"); //form that holds the inputs that's used to change account information
-const name = document.getElementById("name"); //inputted name
 const username = document.getElementById("username"); //inputted username
+//apparently the user has no name (its never registered )
 const password = document.getElementById("password"); //inputted password
 const password2 = document.getElementById("password2"); //repeated password
 const address = document.getElementById("address"); //inputted address
+const areacode = document.getElementById("areacode"); //inputted areacode
 const phoneNumber = document.getElementById("phone"); //inputted phoneNumber
+const city = document.getElementById("city"); //inputted city
 const errorMessages = document.getElementById("errorMessage"); //div that holds error messages
 const changeButton = document.getElementById("changeButton");
-const nameBox  = document.getElementById("nameBox");
-const usernameBox  = document.getElementById("usernameBox");
-const passwordBox  = document.getElementById("passwordBox");
+const nameBox = document.getElementById("nameBox");
+const usernameBox = document.getElementById("usernameBox");
+const passwordBox = document.getElementById("passwordBox");
 const addressBox = document.getElementById("addressBox");
-const phoneBox  = document.getElementById("phoneBox");
-const boxBox = [nameBox,usernameBox,passwordBox,addressBox,phoneBox] //box that holds all the boxes (used to disable the changeButton when all the boxes are hidden
+const phoneBox = document.getElementById("phoneBox");
+const boxBox = [nameBox, usernameBox, passwordBox, addressBox, phoneBox] //box that holds all the boxes (used to disable the changeButton when all the boxes are hidden
 let message = ""; //Array that holds errorMessages
-let toChange = []; //stores the variables that the user want to change to.
+let itemsToSubmit = []; // creates an empty array
 
 /**
  * Blir skickad hit om alla värderingar stämmer
  * här skickar man vidare värdena som ska ändras till java delen
+ * @param valid {array} array that holds all the data the user wants to change
  */
-function success() {
+function success(valid) {
     // TODO: connect to java that sends back a success message
-   let x =   alert("Success");
-    if(x === true){ //if user presses ok ? i think
-    window.location.reload();
+
+    //send valid to java
+    console.log(valid); // TODO: remove later just for test !
+    itemsToSubmit = [] //empties array
+
+    let x = alert("Success");
+    if (x === true) { //if user presses ok ? i think
+        window.location.reload();
     }
 }
 
 /**
  * skickar att användaren vill radera sit account, låter sedan java göra resten
  */
-function removeAccount(){
-  let x =  confirm("Vill du verkligen radera ditt account?")
-  if(x===true){
-    // TODO: connect on java side, (checking what account this is form localstorage is unsafe!)
-    location.assign("../index.html"); //just sends the user back to the main page for now
+function removeAccount() {
+    let x = confirm("Vill du verkligen radera ditt account?")
+    if (x === true) {
+        // TODO: connect on java side, (checking what account this is form localstorage is unsafe!)
+        location.assign("../index.html"); //just sends the user back to the main page for now
 
-  } else{
-    console.log("User did not delete account!")
-  }
+    } else {
+        console.log("User did not delete account!")
+    }
 }
 
-function isAllHidden(){
-  function hidden(){
-    let isHidden = true;
-    for(let i = 0;  i <  boxBox.length;i++){
-      if(boxBox[i].style.display === "block"){
-        isHidden = false;
-      }
-    }
-    return isHidden;
-  }
+/**
+ *
+ * @returns {boolean}
+ */
+function isAllHidden() {
 
-  return !!hidden();
 
 }
 
@@ -63,84 +64,68 @@ function isAllHidden(){
  * @param div - är div boxen som håller i formuläret vi vill visa eller dölja
  * @param input - inmatnings fältet vi vill kunna ändra om man behöver ha data i den eller inte, också ta bart dens värde
  */
-function show(div,input){
-  const formContent =document.getElementById(div);
-  const inputContent = document.getElementById(input);
+function show(div, input) {
+    const formContent = document.getElementById(div);
+    const inputContent = document.getElementById(input);
 
-  if(formContent.style.display === "none"){
-    inputContent.required = true;
-    formContent.style.display = "block";
-  } else {
-    inputContent.required = false;
-    formContent.style.display = "none";
-  }
-  changeButton.disabled = isAllHidden(); //changes the disable status of the changeButton depending on if
-  inputContent.value ="";//should clear the input on show and hide
+    if (formContent.style.display === "none") {
+        inputContent.required = true;
+        formContent.style.display = "block";
+    } else {
+        inputContent.required = false;
+        formContent.style.display = "none";
+    }
+    inputContent.value = "";//should clear the input on show and hide
 }
 
 /**
  * Sanitising the string, so that all html tags get removed
- * @param {String} originalString - original string
- * @returns - Returns a sanitised string
+ * @returns {String} returns a sanitized string
+ * @param {HTMLElement} string that needs to be sanitized
  */
-function removeHtml(originalString){
-  return originalString.replace(/(<([^>]+)>)/gi, "");
+function removeHtml(string) {
+    return string.toString().replace(/(<([^>]+)>)/gi, "");
 }
 
-/**
- * kontrollerar om name stämmer med regexen 
- * @param {String} name
- * @returns {boolean} true om name stämmer med regexen och false om name inte stämmer med regexen
- */
-function validateName(name) {
-  //TODO: change to what register account uses
-  const regex = /^[a-zA-ZäöåÄÖÅ\- ]+$/;
-
-  if (regex.test(name)) {
-    return true;
-  } else {
-    message = (name + " did NOT adhere to the naming standard of this site");
-    return false;
-  }
-}
 
 /**
- * kontrollerar om username (email) stämmer med regexen  
+ * kontrollerar om username (email) stämmer med regexen
  * @param {String} username
  * @returns {boolean} true om username stämmer med regexen och false om username inte stämmer med regexen
  */
 function validateUsername(username) {
-  const regex = new RegExp(
-    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-  ); //adheres to 99.99% of all email addresses in actual use today. and is based on RFC 2822  https://tools.ietf.org/html/rfc2822#section-3.4.1
+    const regex = new RegExp(
+        "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+    ); //adheres to 99.99% of all email addresses in actual use today. and is based on RFC 2822  https://tools.ietf.org/html/rfc2822#section-3.4.1
 
-  if (regex.test(username)) {
-    return true;
-  } else {
-    message = (username + " är inte en standard email address");
-    return false;
-  }
+    if (regex.test(username)) {
+        return true;
+    } else {
+        message += (username + " är inte en standard email address");
+        return false;
+    }
 }
 
 /**
- * kontrollerar om username (email) stämmer med regexen  
+ * kontrollerar om username (email) stämmer med regexen
  * @param {String} password
  * @returns {boolean} true om password stämmer med regexen och false om password inte stämmer med regexen
  */
 function validatePassword(password) {
-  const regex = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,40})"
-  ); //Password need to be at least 6 characters but max 40, include 1 special character, 1 uppercase character and a number
-  //do not by mistake format the regex string changes on format and probably breaks !
-  if (regex.test(password)) {
-    return true;
-  } else {
-    message = (
-      " did not adhere to password standard of the site "
-    );
-    return false;
-  }
+    const regex = new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,40})"
+    ); //Password need to be at least 6 characters but max 40, include 1 special character, 1 uppercase character and a number
+    //do not by mistake format the regex string changes on format and probably breaks !
+    if (regex.test(password)) {
+        return true;
+    } else {
+        message += (
+            " did not adhere to password standard of the site "
+        );
+        return false;
+    }
 }
+
 
 /**
  * kontrollerar om field är 4 nummer mellan 1-9
@@ -148,8 +133,8 @@ function validatePassword(password) {
  * @returns {boolean} true om field stämmer med regexen och false om field inte stämmer med regexen
  */
 function validateAreaCode(field) {
-  const regex = /^[1-9]\d{4}/g;
-  return regex.test(field.val());
+    const regex = /^[1-9]\d{4}/g;
+    return regex.test(field);
 }
 
 /**
@@ -158,24 +143,44 @@ function validateAreaCode(field) {
  * @returns {boolean} true om field stämmer med regexen och false om field inte stämmer med regexen
  */
 function validatePhoneNumber(field) {
-  const regex = /^0\d{6,}/g;
-  return regex.test(field.val());
+    const regex = /^0\d{6,}/g;
+    return regex.test(field);
 }
 
 /**
- * Kollar att field har något värde 
+ * Kollar att field har något värde
  * @param {String} field
  * @returns {boolean} true om field har ett värde och false om field inte har ett värde
  */
 function fieldNotEmpty(field) {
-  return field.val().length > 0;
+    return field.length > 0;
 }
 
 /**
  * checks the form validation
- * @return {boolean} true if the validation was successful and false if it failed
+ * @return {boolean}
  */
 function validation() {
+    const values = [username, password, phoneNumber, areacode, address, city];
+    const valid = [validateUsername(removeHtml(values[0].value)), validatePassword(removeHtml(values[1].value)),
+        validatePhoneNumber(removeHtml(values[2].value)), validateAreaCode(removeHtml(values[3].value)),
+        fieldNotEmpty(removeHtml(values[4].value)), fieldNotEmpty(removeHtml(values[5].value))];
+
+    if (valid.some(boolean => boolean === true)) {
+        if (valid.every((boolean) => boolean === true)) {
+            for (let i = 0; i < valid.length; i++) {
+                if (valid[i] === true) {
+                    itemsToSubmit.push(values[i].value);
+                }
+            }
+            if (itemsToSubmit.length > 0) {
+                return true;
+            }
+        }
+    } else {
+        return false;
+
+    }
 
 }
 
@@ -183,18 +188,11 @@ function validation() {
  * när användaren trycker på submit knappen så callas validation() och samlas error messages
  */
 form.addEventListener("submit", (e) => {
-  //a double check
-  
-  if (username.length === 0 || password.length === 0) {
-    message = ("username and password can't be blank or null");
-  }
-
-  if (validateUsername(removeHtml(username.value)) && validatePassword(removeHtml(password.value))) {
-    //if all validations return true
-    success();
-  }
-  if (message.length > 0) {
     e.preventDefault(); //prevents page reload on submit
-    errorMessages.innerHTML = message.join("\n , ");
-  }
+    if (validation()) {
+        success(itemsToSubmit);
+    }
+    if (message.length > 0) {
+        errorMessages.innerHTML = message;
+    }
 });
