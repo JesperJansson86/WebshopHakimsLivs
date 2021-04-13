0d8fa07c89c7c7ecbd3d4fb310efea22973b778e
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-13 15:18:06 +0200

    Need to figure out SQL injection prevention! but a good start

--- Java/Login.java
+++ Java/Login.java
@@ -16,52 +16,63 @@ import java.util.regex.Pattern;
 public class Login {
 
     private static final ArrayList<String> errors = new ArrayList<>();
-    //test login
+    //test logins
     private static final String testUsername = "lukas@gmail.com";
     private static final String testPassword = "Password1!";
+    private static final String badUsername = "lukas";
+    private static final String badPassword = "password";
 
     public static void main(String[] args) {
         //TODO: Remove main form login ! ITS ONLY FOR TEST!
-
         validate(testUsername, testPassword);
-
-        System.out.println(errors);
-
+        validate(badUsername, badPassword);
     }
 
     public static void validate(String username, String password) {
         if (fieldNotEmpty(username) && fieldNotEmpty(password)) {
-            validateUsername(removeBadCharacters(username));
-            validatePassword(removeBadCharacters(password));
+            final String newUsername = removeBadCharacters(username);
+            final String newPassword = removeBadCharacters(password);
+            if (fieldNotEmpty(newUsername) && fieldNotEmpty(newPassword)) { //checks that the removeBadCharacters did not remove everything
+                if (validateUsername(newUsername) && validatePassword(newPassword)) {
+                    System.out.println("\nUsername = " + newUsername + "| Password = " + newPassword); //TODO: IS FOR TESTING ONLY, SEND newUsername & newPassword TO THE NEXT CLASS
+                } else {
+                    errors.add("\nUsername && password was not valid! ");
+                }
+            } else {
+                errors.add("\nInput included only Illegal characters! ");
+            }
         } else {
             errors.add("\nInput empty! ");
         }
-
+        System.out.println(errors);
     }
 
-    private static void validateUsername(String username) {
+    private static boolean validateUsername(String username) {
         final Pattern regex = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
         final Matcher matcher = regex.matcher(username);
 
         if (matcher.matches()) {
-            System.out.println("\n" + username + " matches");
+            System.out.println("\n" + username + " matches! ");
+            return true;
         } else {
-            System.out.println("\n" + username + " dose not match");
-            errors.add("\n" + username + " did not adhere to username pattern ");
+            System.out.println("\n" + username + " dose not match! ");
+            errors.add("\n" + username + " did not adhere to username pattern! ");
+            return false;
         }
 
     }
 
-    private static void validatePassword(String password) {
+    private static boolean validatePassword(String password) {
         final Pattern regex = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,40})");
         final Matcher matcher = regex.matcher(password);
 
         if (matcher.matches()) {
-            System.out.println("\n" + password + " matches");
+            System.out.println("\n" + password + " matches! ");
+            return true;
         } else {
-            System.out.println("\n" + password + " dose not match");
+            System.out.println("\n" + password + " dose not match! ");
             errors.add("\nPassword did not adhere to password pattern ");
-
+            return false;
         }
     }
 
@@ -69,7 +80,6 @@ public class Login {
         String temp = string;
         temp = removeHtml(temp);
         temp = removeSQL(temp);
-        temp = removeOther(temp);
         return temp;
     }
 
@@ -86,7 +96,7 @@ public class Login {
         }
     }
 
-    //todo: remove harmful sql code before it reaches the database!
+    //todo: remove all SQL code before it reaches the database!
     private static String removeSQL(String string) {
         final String regex = "";
         final Pattern pattern = Pattern.compile(regex);
@@ -94,20 +104,6 @@ public class Login {
 
         if (matcher.find()) {
             errors.add("\nInputted String included SQL elements! ");
-            return string.replaceAll(regex, "");
-        } else {
-            return string;
-        }
-    }
-
-    //todo: remove other harmful code (have not yet figured it out !
-    private static String removeOther(String string) {
-        final String regex = "";
-        final Pattern pattern = Pattern.compile(regex);
-        final Matcher matcher = pattern.matcher(string);
-
-        if (matcher.find()) {
-            errors.add("\nInputted String included bad elements! ");
             return string.replaceAll(regex, "");
         } else {
             return string;
