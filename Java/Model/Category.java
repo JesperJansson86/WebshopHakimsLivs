d31978dedc869ef4df2b44a20afdea4d6c99a756
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:14:01 +0200

    Created a Category Object with the help of Lombok

---
+++ Java/Model/Category.java
@@ -0,0 +1,46 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 16:11
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String category;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "category='" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category1 = (Category) o;
+        return getCategory().equals(category1.getCategory());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCategory());
+    }
+}
