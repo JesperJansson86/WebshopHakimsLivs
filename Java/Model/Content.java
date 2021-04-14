f96dba502a719720c3b7a1335d21425dc9e5c2e5
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:37:32 +0200

    Created a Content Object with the help of Lombok

---
+++ Java/Model/Content.java
@@ -0,0 +1,47 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 16:35
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ **/
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Content {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String requirement;
+
+    @Override
+    public String toString() {
+        return "Content{" +
+                "requirement='" + requirement + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Content)) return false;
+        Content content = (Content) o;
+        return getRequirement().equals(content.getRequirement());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRequirement());
+    }
+}
