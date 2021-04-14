9b2e378810c0d82ec4e52507170f0bb24365f7bf
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:02:53 +0200

    Created a City Object with the help of Lombok (Records not supported by the Harmony or whatever the database solution we are gonna use is called)

---
+++ Java/Model/City.java
@@ -0,0 +1,47 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 15:49
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ **/
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class City {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String city;
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+        City city1 = (City) o;
+        return getCity().equals(city1.getCity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity());
+    }
+}
