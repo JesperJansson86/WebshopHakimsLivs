e029b69733efb09b775b1203b5f9d1275e54a085
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:11:36 +0200

    Created a Unit Object with the help of Lombok

---
+++ Java/Model/Unit.java
@@ -0,0 +1,49 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 16:08
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+public class Unit {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String unit;
+    @NonNull
+    private String longUnit;
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "unit='" + unit + '\'' +
+                ", longUnit='" + longUnit + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Unit)) return false;
+        Unit unit1 = (Unit) o;
+        return getUnit().equals(unit1.getUnit()) && getLongUnit().equals(unit1.getLongUnit());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUnit(), getLongUnit());
+    }
+}
