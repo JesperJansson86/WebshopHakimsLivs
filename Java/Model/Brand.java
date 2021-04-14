82df99c32a7f3f05521b3df6c541d85a4cdfabf7
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:08:48 +0200

    Created a Brand Object with the help of Lombok

---
+++ Java/Model/Brand.java
@@ -0,0 +1,46 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/*
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 16:03
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class Brand {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String brand;
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Brand)) return false;
+        Brand brand1 = (Brand) o;
+        return getBrand().equals(brand1.getBrand());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand());
+    }
+}
