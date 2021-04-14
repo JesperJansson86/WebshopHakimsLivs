d49006cdc0272dfb43f99fbbc42e2fdef05561a2
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-14 16:18:45 +0200

    Created a AreaCode Object with the help of Lombok

---
+++ Java/Model/AreaCode.java
@@ -0,0 +1,49 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/04/2021
+ * Time: 16:16
+ * Project: WebshopHakimsLivs
+ * Copyright: MIT
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+public class AreaCode {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String areaCode;
+    @NonNull
+    private City city;
+
+    @Override
+    public String toString() {
+        return "AreaCode{" +
+                "areaCode='" + areaCode + '\'' +
+                ", city=" + city +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AreaCode)) return false;
+        AreaCode areaCode1 = (AreaCode) o;
+        return getAreaCode().equals(areaCode1.getAreaCode()) && getCity().equals(areaCode1.getCity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAreaCode(), getCity());
+    }
+}
